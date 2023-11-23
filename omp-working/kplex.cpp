1a541d0cd14cb999206f6760ab02304f2465a1d1
akhlaqueak <akhlaque.ak@gmail.com>
2023-11-24 00:29:03 +0300

    uploading...

--- omp-working/kplex.cpp
+++ omp-working/kplex.cpp
@@ -9,7 +9,7 @@
 #define LOOKAHEAD
 #define CTCP
 // time theshold in microseconds...
-#define TIMEOUT_THRESH 100000
+#define TIMEOUT_THRESH 100
 bool isTimeout(auto start_t)
 {
     return duration_cast<microseconds>(steady_clock::now() - start_t).count() > TIMEOUT_THRESH;
