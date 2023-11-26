1b07acbbf36e3873d4f3e3caa2793f8fa77a453f
akhlaqueak <akhlaque.ak@gmail.com>
2023-11-26 15:41:00 +0300

    uploading...

--- ours-gi/kplex.cpp
+++ ours-gi/kplex.cpp
@@ -1557,9 +1557,9 @@ int main(int argc, char *argv[])
     EnumKPlex kp(g, k1, k2, q);
     kp.enumerate();
     if (CUTOFF)
-        cout << sr << file << " Timed Out" << endl;
+        cout << sr << " " << file << " Timed Out" << endl;
     else
-        cout << sr << file << " Enumeration time (ms): " << chrono::duration_cast<chrono::milliseconds>(TIME_NOW - tick).count() << endl;
+        cout << sr << " " << file << " Enumeration time (ms): " << chrono::duration_cast<chrono::milliseconds>(TIME_NOW - tick).count() << endl;
 
     cout << endl;
     return 0;
